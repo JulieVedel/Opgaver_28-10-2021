7e4e3eea713ba22263c426d81104081ed743d806
JulieVedel <juli76k0@edu.easj.dk>
2021-10-28 09:59:50 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.format("Indtast den %d. værdi.\n", i + 1);
+
+            array[i] = scanner.nextInt();
+        }
+        print(array);
+        gennemsnit(array, array.length);
+    }
+
+    public static void print(int[] array) {
+        System.out.println("Værdierne i arrayet er: " + Arrays.toString(array));
+    }
+
+    public static void gennemsnit(int[] array, int count) {
+        double sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        System.out.println("Gennemsnittet af værdierne i arrayet er: " + (sum / count));
+    }
+}
